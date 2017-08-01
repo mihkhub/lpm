158cd6817a8f8d519e79d0ccb821cc75b403be1d
Issac <changfeng.or.chen79@gmail.com>
2017-08-01 09:53:13 +0800

    bsearch - binary search of a sorted array

--- 3/bsearch/bsearch.c
+++ 3/bsearch/bsearch.c
@@ -1,3 +1,7 @@
+/*
+ * bsearch - binary search of a sorted array
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,6 +17,9 @@ static int compmi(const void *m1, const void *m2) {
 	struct mi *mi2 = (struct mi *) m2;
 	return strcmp(mi1->name, mi2->name);
 }
+int compar(const void * s1, const void *s2);
+
+int compar_usage(); 
 int main(int argc, char **argv) {
 	int i;
 	qsort(months, nr_of_months, sizeof(struct mi), compmi);
@@ -25,5 +32,35 @@ int main(int argc, char **argv) {
 		else
 			printf("%s: month #%d\n", res->name, res->nr);
 	}
+
+	compar_usage(); 
+	exit(EXIT_SUCCESS);
+}
+int compar(const void * s1, const void *s2)
+{
+	int *a = (int*)s1;
+	int *b = (int*)s2;
+	return *a < *b?-1:*a > *b;
+}
+
+int compar_usage() 
+{
+	int a = 3;
+	int b = 4;
+	int ret = 0;
+
+	ret = compar(&a, &b);
+	printf("%d ? %d = %d\n",a, b, ret);
+
+	a = 7;
+	b = 7;
+	ret = compar(&a, &b);
+	printf("%d ? %d = %d\n",a, b, ret);
+
+	a = 9;
+	b = 8;
+	ret = compar(&a, &b);
+	printf("%d ? %d = %d\n",a, b, ret);
+
 	exit(EXIT_SUCCESS);
 }
