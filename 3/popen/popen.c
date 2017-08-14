998dde6e680fe3e5f96c666db017d649c9bf6379
Issac <changfeng.or.chen79@gmail.com>
2017-08-14 11:28:41 +0800

    heap and stack buffer

--- 3/popen/popen.c
+++ 3/popen/popen.c
@@ -4,12 +4,19 @@
 int main(int argc, char **argv)
 {
 	size_t n = 1024;
-	char *line = (char *)malloc(n);
+	char *line1 = (char *)malloc(n);
+	char buf[1024];
+	char * line = buf;
 	const char cmd[] = "uname -r";
+	const char cmd1[] = "uname -m";
 	FILE * f = popen(cmd, "r");	
 	getline(&line, &n, f);	
-	printf("%s",line);
+	printf("%s\n",line);
 	pclose(f);
-	free(line);
+	/* heap buffer */
+	FILE * f1 = popen(cmd1, "r");	
+	getline(&line1, &n, f1);	
+	printf("%s\n",line1);
+	pclose(f1);
 	return 0;
 }
