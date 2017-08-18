1f56ac1dfae220db2a874a3e2e4a5b10a0e744fc
Issac <changfeng.or.chen79@gmail.com>
2017-08-18 10:29:09 +0800

    Update strerror_r comment

--- 3/strerror/strerror.c
+++ 3/strerror/strerror.c
@@ -9,7 +9,7 @@ int main(int argc, char **argv)
 	char buf[64];
 	size_t buflen = 64;
 	printf("strerror: %s\n", strerror(1));
-	strerror_r(1, buf, buflen); // thread safe, XSI-compilant is default.
-	printf("strerror_r: %s\n", buf);
+	int ret = strerror_r(1, buf, buflen); // thread safe, XSI-compilant is default. It returns the error string in the user-supplied buffer buf of length buflen.
+	printf("strerror_r: %s ret=%d\n", buf, ret);
 	return 0;	
 }
